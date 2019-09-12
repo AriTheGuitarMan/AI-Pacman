445d32b0b4b78162fc2c161b68466e5a99c52545
Ariel Wald <15020608+AriTheGuitarMan@users.noreply.github.com>
2019-09-12 16:30:31 -0400

    Python Warmup Challenge
    Committed Changes on Python Warmup Challenge

---
+++ Python Warmup Challenge/tutorial/shopSmart.py
@@ -0,0 +1,58 @@
+# shopSmart.py
+# ------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+Here's the intended output of this script, once you fill it in:
+
+Welcome to shop1 fruit shop
+Welcome to shop2 fruit shop
+For orders:  [('apples', 1.0), ('oranges', 3.0)] best shop is shop1
+For orders:  [('apples', 3.0)] best shop is shop2
+"""
+
+import shop
+#import collections
+#from operator import itemgetter
+
+def shopSmart(orderList, fruitShops):
+    """
+        orderList: List of (fruit, numPound) tuples
+        fruitShops: List of FruitShops
+    """
+    "*** YOUR CODE HERE ***"
+    #s is for shops, costList has the list of order costs from each respective FruitShop
+    costList = {}
+    for s in fruitShops: #putting the cost of every fruitShop from orderList into the costList
+        costList[s] = s.getPriceOfOrder(orderList)
+    #od = collections.OrderedDict(sorted(d.items()))
+    #return od[0]
+    #costList.sort(key=costList.get)
+    #return costList[0]
+
+    #sort the costList in increasing format so that the lowest value is at the first index
+    #return (sorted(costList.keys(), key=operator.itemgetter(0), reverse=False))[0]
+    return (sorted(costList.keys(), key=costList.get, reverse=False))[0]
+    return None
+
+if __name__ == '__main__':
+  "This code runs when you invoke the script from the command line"
+  orders = [('apples',1.0), ('oranges',3.0)]
+  dir1 = {'apples': 2.0, 'oranges':1.0}
+  shop1 =  shop.FruitShop('shop1',dir1)
+  dir2 = {'apples': 1.0, 'oranges': 5.0}
+  shop2 = shop.FruitShop('shop2',dir2)
+  shops = [shop1, shop2]
+  print "For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName()
+  orders = [('apples',3.0)]
+  print "For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName()
